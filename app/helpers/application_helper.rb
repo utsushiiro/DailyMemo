78716ab95709c48fd91163dfcc5563f9e28a5f90
utsushiiro <ozeki.haru@gmail.com>
2017-09-06 03:23:18 +0900

    typoを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title='')
-    base_title = "DairyMemo"
+    base_title = "DailyMemo"
     if page_title.empty?
       base_title
     else
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "DairyMemo"
-    assert_equal full_title("Help"), "Help | DairyMemo"
+    assert_equal full_title,         "DailyMemo"
+    assert_equal full_title("Help"), "Help | DailyMemo"
   end
 end
