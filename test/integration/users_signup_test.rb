bf66499de28ba078a3968a54e8e2f38b23fc0144
utsushiiro <ozeki.haru@gmail.com>
2017-09-09 23:43:36 +0900

    ユーザ登録後にログインした状態になるように変更

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_url
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: "", email: "user@invalid",
-                                password: "foo", password_confirmation: "bar"} }
+      post signup_path, params: { user: { name: '', email: 'user@invalid',
+                                password: 'foo', password_confirmation: 'bar'} }
 
     end
     assert_template 'users/new'
@@ -14,16 +14,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors', count: 8
   end
 
-  test "valid signup information" do
+  test 'valid signup information' do
     get signup_url
     assert_difference 'User.count', 1 do
-      post signup_path,  params: { user: { name: "Example User",
-                                           email: "user@example.com",
-                                           password:              "password",
-                                           password_confirmation: "password" } }
+      post signup_path,  params: { user: { name: 'Example User',
+                                           email: 'user@example.com',
+                                           password:              'password',
+                                           password_confirmation: 'password' } }
     end
     follow_redirect!
     assert_template 'users/show'
     assert flash[:success]
+    assert is_logged_in?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -10,4 +10,7 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
