10fd26c596a4175c4902b94410aae6dcd82db952
utsushiiro <ozeki.haru@gmail.com>
2017-09-10 22:33:10 +0900

    ログイン状態のremember機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,40 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token
+
   validates :name,  presence: true, length: { maximum: 50 }
+
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def remember
+    self.remember_token = User.create_remember_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    self.remember_token = nil
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
+    end
+
+    def create_remember_token
+      SecureRandom.urlsafe_base64
     end
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -32,5 +32,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
+
+    # simulate the logout action after a user logged in by two different windows
+    # and logged out  in one of  window
+    delete logout_path
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
   end
 end
