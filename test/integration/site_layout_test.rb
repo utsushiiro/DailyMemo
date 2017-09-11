88b9afcdb10aa7e50cb5872772a222c0dd96e5f3
utsushiiro <ozeki.haru@gmail.com>
2017-09-12 00:00:14 +0900

    レイアウト内のリンク確認のテストを修正

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+  end
+
   test 'layout links' do
+    # not logged in
     get root_path
-    assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
-    get signup_path
-    assert_select 'title', full_title('Sign up')
+    assert_select 'a[href=?]', login_path
+
+    # logged in
+    log_in_as(@user)
+    follow_redirect!
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
   end
 end
