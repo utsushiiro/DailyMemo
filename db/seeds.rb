833fd1aba50e1d4f20ea4b739bc50e3a910d7d37
utsushiiro <ozeki.haru@gmail.com>
2017-09-12 03:04:56 +0900

    Userモデルにadmin属性を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name: 'example',
              email: 'example@example.com',
              password: 'password',
-             password_confirmation: 'password')
+             password_confirmation: 'password',
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: 'foo', email: 'bar' }}
     assert_redirected_to root_url
   end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: '',
+                                                    password_confirmation: '',
+                                                    admin: !@other_user.admin? }}
+    assert_not @other_user.reload.admin?
+  end
 end
