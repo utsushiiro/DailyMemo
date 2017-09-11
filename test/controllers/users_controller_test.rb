6c9b4aff0598aaeed9dd67314967d961bd3b1cab
utsushiiro <ozeki.haru@gmail.com>
2017-09-11 21:46:06 +0900

    ユーザ情報の編集のテストに正しいユーザかどうかのテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:test_user)
+    @other_user = users(:another_test_user)
   end
 
   test 'should get new' do
@@ -20,5 +21,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: 'foo', email: 'bar' }}
     assert_not_empty flash[:danger]
     assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as a wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as a wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: 'foo', email: 'bar' }}
+    assert_redirected_to root_url
   end
 end
