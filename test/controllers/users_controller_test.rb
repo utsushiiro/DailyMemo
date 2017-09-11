26a2c1decc068e3f00729111487f4c4d438bd7fe
utsushiiro <ozeki.haru@gmail.com>
2017-09-11 21:20:50 +0900

    ユーザ情報の編集にログインが必要となるように修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+  end
+
   test 'should get new' do
     get signup_url
     assert_response :success
+  end
+
+  test 'should redirect edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not_empty flash[:danger]
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: { user: { name: 'foo', email: 'bar' }}
+    assert_not_empty flash[:danger]
+    assert_redirected_to login_url
   end
 end
