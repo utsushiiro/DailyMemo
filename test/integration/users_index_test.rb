c9850aaa1734255b9d57a31420fe1ebbbc6bb715
utsushiiro <ozeki.haru@gmail.com>
2017-09-12 02:24:00 +0900

    ユーザ一覧をページネーションで表示するように変更

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,12 +5,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @user = users(:test_user)
   end
 
-  test 'users list' do
+  test 'index including pagina' do
     log_in_as(@user)
-    follow_redirect!
-    assert_select 'a[href=?]', users_path
     get users_path
     assert_template 'users/index'
-    assert_select 'ul.users>li>a[href=?]', user_path(@user), 'test_user'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      assert_select 'ul.users a[href=?]', user_path(user), text: user.name
+    end
   end
 end
