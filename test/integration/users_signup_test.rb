0c25c1278a393e8ac8d3380e8ed3c87a58fbe197
utsushiiro <ozeki.haru@gmail.com>
2017-09-12 23:55:17 +0900

    ユーザ登録の統合テストをアカウント有効化を考慮したものに修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'invalid signup information' do
     get signup_url
     assert_select 'form[action="/signup"]'
@@ -22,9 +27,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password:              'password',
                                            password_confirmation: 'password' } }
     end
+    assert_redirected_to root_url
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    # try to log in when not activated
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    # follow a bad activation link(incorrect token)
+    get edit_account_activation_url('Invalid token', email: user.email)
+    assert_not user.reload.activated?
+    assert_not is_logged_in?
+
+    # follow a bad activation link(incorrect email)
+    get edit_account_activation_url(user.activation_token, email: 'Wrong Email')
+    assert_not is_logged_in?
+
+    # follow the correct link
+    get edit_account_activation_url(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    assert is_logged_in?
+    assert_redirected_to user
     follow_redirect!
-    # assert_template 'users/show'
-    # assert flash[:success]
-    # assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
