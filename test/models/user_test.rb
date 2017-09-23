3edfd15fdfa42b107afd838d8064e9cc54c61d06
utsushiiro <ozeki.haru@gmail.com>
2017-09-24 00:18:56 +0900

    ユーザ削除時にそのユーザのポストも削除するように変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,12 @@ class UserTest < ActiveSupport::TestCase
     @user.remember_digest = nil
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test 'associated microposts should be destroyed' do
+    @user.save
+    @user.microposts.create!(content: 'hoge')
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
