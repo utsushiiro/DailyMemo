1d821fcecf6dc87b49fb4ada450ccc9cb64c58c7
utsushiiro <ozeki.haru@gmail.com>
2017-09-24 00:03:26 +0900

    Micropost取得の際の順序を作成日時の降順に設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,6 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  default_scope -> { order(created_at: :desc) }
 end
