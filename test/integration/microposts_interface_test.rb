345f507b210e632672dde9c9504ff7b9029dd710
utsushiiro <ozeki.haru@gmail.com>
2017-09-25 01:57:51 +0900

    Micropostに関する操作の統合テストを作成

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:test_user)
+  end
+
+  test 'micropost interface' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+
+    # invalid post
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: '' } }
+    end
+    assert_select 'div#error_explanation'
+
+    # valid post
+    content = 'hoge'
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+
+    # delete post
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+
+    # confirm that no delete link exists in other user's profile
+    get user_path(users(:another_test_user))
+    assert_select 'a', text: 'delete', count: 0
+  end
+end
