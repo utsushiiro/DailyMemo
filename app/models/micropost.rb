048fb5cf0cae27d17fef1d593b32890030f12916
utsushiiro <ozeki.haru@gmail.com>
2017-09-25 15:57:26 +0900

    添付画像に対するバリデーションを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,8 +3,17 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
 
   default_scope -> { order(created_at: :desc) }
 
   mount_uploader :picture, PictureUploader
+
+  private
+
+  def picture_size
+    if picture.size > 2.megabytes
+      self.errors.add(:picture, 'should be less than 2MB')
+    end
+  end
 end
